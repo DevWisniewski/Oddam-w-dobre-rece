2ff3f96fb0a5be610e57424b474571f964985eb2
DevWisniewski <grzegorz.wisniewski.office@gmail.com>
2024-04-08 14:19:06 +0200

    Zmieniono pliki html by kożystały z base.html

--- OddamApp/views.py
+++ OddamApp/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def register(request):
+    return render(request, 'register.html')
+
+def form(request):
+    return render(request, 'form.html')
+
+def form_confirmation(request):
+    return render(request, 'form-confirmation.html')
+
+
+# to sa widoki testowe
+def form1(request):
+    return render(request, 'form1.html')
+
+def form_confirmation1(request):
+    return render(request, 'form-confirmation1.html')
+
+def index1(request):
+    return render(request, 'index1.html')
+
+def login1(request):
+    return render(request, 'login1.html')
+
+def register1(request):
+    return render(request, 'register1.html')
